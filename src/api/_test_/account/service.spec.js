07c5649215cfc3d3771e49ae218966cf96c33097
bianca.pereira <bianca.pereira@dextra-sw.com>
2020-08-29 17:55:10 -0300

    test: add test to update balance

--- src/api/_test_/account/service.spec.js
+++ src/api/_test_/account/service.spec.js
@@ -1,5 +1,11 @@
 import bankingAccountService from '../../account/service';
-import { createMockClientBody } from '../_mocks_/bodyMock';
+import bankingAccountModel from '../../account/model';
+import {
+    createMockClientBody,
+    createMockBankingAccountBody,
+    createMockDepositBody,
+    createMockWithdrawBody
+} from '../_mocks_/bodyMock';
 
 describe('test validateBankingAccount service', () => {
     it('return false when fullName is null', async () => {
@@ -42,5 +48,49 @@ describe('test validateBankingAccount service', () => {
         const output = bankingAccountService.validateBankingAccount(m);
         expect(output).toBeFalsy();
         expect(m.document).toEqual("");
+    });
+});
+
+describe('test updateAvailableBalance service', () => {
+    const mockFindOneAndUpdate = bankingAccountModel.findOneAndUpdate = jest.fn();
+
+    it('actionType A return bigger availableBalance', async () => {
+        mockFindOneAndUpdate.mockReturnValueOnce({
+            _id: "5f4955c461fe774ff21bbfc0",
+            clientCode: "5f4955c361fe774ff21bbfbf",
+            branchNumber: "0001",
+            branchNumberDigit: "0",
+            accountNumber: "543190",
+            accountNumberDigit: "0",
+            fullAccountNumber: "543190-0",
+            availableBalance: "1056.72"
+        });
+        const mockBankingAccount = createMockBankingAccountBody({});
+        const mockDepositAccount = createMockDepositBody({});
+        const output = await bankingAccountService.updateAvailableBalance(mockDepositAccount, mockBankingAccount);
+        expect(output).toEqual({ "_id": "5f4955c461fe774ff21bbfc0", "accountNumber": "543190", "accountNumberDigit": "0", "availableBalance": "1056.72", "branchNumber": "0001", "branchNumberDigit": "0", "clientCode": "5f4955c361fe774ff21bbfbf", "fullAccountNumber": "543190-0" });
+        expect(mockBankingAccount.availableBalance).toEqual(535.36);
+        expect(mockDepositAccount.value).toEqual(521.36);
+        expect(output.availableBalance).toEqual("1056.72");
+    });
+
+    it('actionType D return less availableBalance', async () => {
+        mockFindOneAndUpdate.mockReturnValueOnce({
+            _id: "5f4955c461fe774ff21bbfc0",
+            clientCode: "5f4955c361fe774ff21bbfbf",
+            branchNumber: "0001",
+            branchNumberDigit: "0",
+            accountNumber: "543190",
+            accountNumberDigit: "0",
+            fullAccountNumber: "543190-0",
+            availableBalance: "498.91"
+        });
+        const mockBankingAccount = createMockBankingAccountBody({});
+        const mockWithdrawAccount = createMockWithdrawBody({});
+        const output = await bankingAccountService.updateAvailableBalance(mockWithdrawAccount, mockBankingAccount);
+        expect(output).toEqual({ "_id": "5f4955c461fe774ff21bbfc0", "accountNumber": "543190", "accountNumberDigit": "0", "availableBalance": "498.91", "branchNumber": "0001", "branchNumberDigit": "0", "clientCode": "5f4955c361fe774ff21bbfbf", "fullAccountNumber": "543190-0" });
+        expect(mockBankingAccount.availableBalance).toEqual(535.36);
+        expect(mockWithdrawAccount.value).toEqual(36.45);
+        expect(output.availableBalance).toEqual("498.91");
     });
 });
